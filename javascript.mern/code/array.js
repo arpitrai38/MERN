8c5045853f79b5cbd47c43c4141e17791e97c8bd
Arpit Rai <sadhanamarendra12@gmail.com>
2026-07-01 12:15:49 +0530

    push operation add

--- javascript.mern/code/array.js
+++ javascript.mern/code/array.js
@@ -1,8 +1,14 @@
-const arr=[1,2,3,4,5]
-arr.forEach
-((a)=>{
-console.log(a);
+// const arr=[1,2,3,4,5]
+// arr.forEach
+// ((a)=>{
+// console.log(a);
 
 
-}
-)
+// }
+// )
+
+
+const arr = [1,23,45,5,6,6,7,]
+arr.push(30)
+// arr.pop
+console.log(arr)
